c45e79d59e00f90e24e492c63ee51a91a26fd1ea
h3ura12 <113536354+h3ura12@users.noreply.github.com>
2022-09-14 08:04:26 -0700

    Add files via upload

---
+++ first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  first_app
+//
+//  Created by Misha Gavura on 09.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
